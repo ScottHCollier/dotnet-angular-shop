13fc52c3bedcd3243b766447b2b96314f3f9aa61
Scott Collier <scott.h.collier@gmail.com>
2022-04-21 23:50:34 +0100

    End of section 18

---
+++ Core/Entities/OrderAggregate/Address.cs
@@ -0,0 +1,26 @@
+namespace Core.Entities.OrderAggregate
+{
+    public class Address
+    {
+        public Address()
+        {
+        }
+
+        public Address(string firstName, string lastName, string street, string city, string postCode, string country)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Street = street;
+            City = city;
+            PostCode = postCode;
+            Country = country;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string PostCode { get; set; }
+        public string Country { get; set; }
+    }
+}
