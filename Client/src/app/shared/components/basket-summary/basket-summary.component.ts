b3c1bb4df1287aa8495ebcb025deefaf33084025
Scott Collier <scott.h.collier@gmail.com>
2022-04-23 01:14:20 +0100

    End of section 20

--- Client/src/app/shared/components/basket-summary/basket-summary.component.ts
+++ Client/src/app/shared/components/basket-summary/basket-summary.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 
 import { BasketService } from '../../../basket/basket.service';
 import { IBasket, IBasketItem } from '../../models/basket';
+import { IOrderItem } from '../../models/order';
 
 @Component({
   selector: 'app-basket-summary',
@@ -10,19 +11,19 @@ import { IBasket, IBasketItem } from '../../models/basket';
   styleUrls: ['./basket-summary.component.scss'],
 })
 export class BasketSummaryComponent implements OnInit {
-  basket$: Observable<IBasket>;
   @Input() isBasket: boolean = true;
+  @Input() isOrder: boolean = false;
+  @Input() items: any[] = [];
+
   @Output() decrement: EventEmitter<IBasketItem> =
     new EventEmitter<IBasketItem>();
   @Output() increment: EventEmitter<IBasketItem> =
     new EventEmitter<IBasketItem>();
   @Output() remove: EventEmitter<IBasketItem> = new EventEmitter<IBasketItem>();
 
-  constructor(private basketService: BasketService) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.basket$ = this.basketService.basket$;
-  }
+  ngOnInit(): void {}
 
   decrementItemQuantity(item: IBasketItem) {
     this.decrement.emit(item);
--- Client/src/app/shared/components/order-totals/order-totals.component.ts
+++ Client/src/app/shared/components/order-totals/order-totals.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BasketService } from '../../../basket/basket.service';
 import { IBasketTotals } from '../../models/basket';
@@ -10,6 +10,9 @@ import { IBasketTotals } from '../../models/basket';
 })
 export class OrderTotalsComponent implements OnInit {
   basketTotals$: Observable<IBasketTotals>;
+  @Input() shippingPrice: number;
+  @Input() subtotal: number;
+  @Input() total: number;
 
   constructor(private basketService: BasketService) {}
 
